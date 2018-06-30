af7cec8ca9fdf993231431f6cfd1f6265c27dc02
imhenoch <isidro.henoch@gmail.com>
2018-06-29 20:29:14 -0500

    UI improved, now some btn and TextView are displayed

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -2,14 +2,27 @@ extern crate gio;
 extern crate gtk;
 
 use gtk::prelude::*;
+use gtk::{ApplicationWindow, Box, Button, Orientation, PackType, ScrolledWindow, TextView};
 
 pub fn build_ui(application: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(application);
+    let window = ApplicationWindow::new(application);
 
     window.set_title("Doodle");
     window.set_border_width(10);
     window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(400, 300);
+    window.set_default_size(700, 500);
 
+    let container = Box::new(Orientation::Vertical, 10);
+    let scroll = ScrolledWindow::new(None, None);
+    let text = TextView::new();
+    let btn2 = Button::new_with_label("Btn 2");
+
+    scroll.add(&text);
+    container.set_homogeneous(false);
+    container.add(&scroll);
+    container.add(&btn2);
+
+    container.set_child_packing(&scroll, true, true, 5, PackType::Start);
+    window.add(&container);
     window.show_all();
 }
