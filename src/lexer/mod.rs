57dfca61c00c60e24e076fbe207e0d0ceab8bb91
imhenoch <isidro.henoch@gmail.com>
2018-06-30 23:06:58 -0500

    Input splited into multiple strings and pushed into some Vec

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub mod string_processor;
 
 pub const INT_TYPE: u8 = 1;
@@ -6,20 +8,26 @@ pub const CHAR_TYPE: u8 = 3;
 pub const BOOL_TYPE: u8 = 4;
 pub const STRING_TYPE: u8 = 5;
 
-pub struct Symbol {
+pub const LIMITERS: [char; 8] = [';', '\n', ' ', '(', ')', '{', '}', ','];
+
+pub struct Lexem {
     pub token: String,
     pub data_type: Option<u8>,
     pub category: Option<u8>,
     pub context: Option<String>,
+    pub column: u32,
+    pub row: u32,
 }
 
-impl Symbol {
-    pub fn new(token: String) -> Symbol {
-        Symbol {
+impl Lexem {
+    pub fn new(token: String, column: u32, row: u32) -> Lexem {
+        Lexem {
             token: token,
             data_type: None,
             category: None,
             context: None,
+            column: column,
+            row: row,
         }
     }
 
@@ -33,5 +41,15 @@ impl Symbol {
 
     pub fn set_context(&mut self, context: String) {
         self.context = Some(context);
+    }
+}
+
+impl fmt::Display for Lexem {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{{token: '{}', column: {}, row: {}}}",
+            self.token, self.column, self.row
+        )
     }
 }
--- src/lexer/string_processor.rs
+++ src/lexer/string_processor.rs
@@ -1,3 +1,55 @@
+use lexer::{Lexem, LIMITERS};
+
 pub fn transform_string_to_collection(input: String) {
-    for character in input.chars() {}
+    let mut lexems: Vec<Lexem> = Vec::new();
+    let mut x0 = 0;
+    let mut x1 = 0;
+    let mut column = 0;
+    let mut row = 0;
+    let len = input.len();
+    let mut chars = input.chars();
+    let mut skip_line = false;
+
+    while x1 < len {
+        let c = chars.next().unwrap();
+        if is_limiter(&c) {
+            if x0 != x1 {
+                let s = input.get(x0..x1).unwrap();
+                if s == "//" {
+                    skip_line = true;
+                } else if !skip_line {
+                    lexems.push(Lexem::new(s.to_string(), column - s.len() as u32, row));
+                }
+                x0 = x1 + 1;
+            } else {
+                x0 += 1;
+            }
+            if c == ';' || c == '(' || c == ')' || c == '{' || c == '}' || c == ',' {
+                lexems.push(Lexem::new(c.to_string(), column as u32, row));
+            }
+            if c == '\n' {
+                skip_line = false;
+                row += 1;
+                column = 0;
+            }
+        }
+        column += 1;
+        x1 += 1;
+    }
+    if x0 != x1 {
+        let s = input.get(x0..x1).unwrap();
+        lexems.push(Lexem::new(s.to_string(), column, row));
+    }
+    for lexem in lexems {
+        println!("lexem: {}", lexem);
+    }
+}
+
+fn is_limiter(limiter: &char) -> bool {
+    for l in LIMITERS.iter() {
+        if limiter == l {
+            return true;
+        }
+    }
+    false
 }
