b2ce1113868e01688344dc5666818ffc71316eaf
imhenoch <isidro.henoch@gmail.com>
2018-07-02 00:01:44 -0500

    id automata changed, now is a bit more elegant

--- src/automatas/id_automata.rs
+++ src/automatas/id_automata.rs
@@ -3,20 +3,21 @@ pub fn is_id(token: &mut String) -> bool {
 }
 
 fn q0(token: &mut String) -> bool {
-    return match token.remove(0) as u32 {
+    match token.remove(0) as u32 {
         // A-Z    _      a-z
         65...90 | 95 | 97...122 => q1(token),
         _ => false,
-    };
+    }
 }
 
 fn q1(token: &mut String) -> bool {
     if token.is_empty() {
-        return true;
+        true
+    } else {
+        match token.remove(0) as u32 {
+            // 0-9      A-Z     _      a-z
+            48...57 | 65...90 | 95 | 97...122 => q1(token),
+            _ => false,
+        }
     }
-    return match token.remove(0) as u32 {
-        // 0-9      A-Z     _      a-z
-        48...57 | 65...90 | 95 | 97...122 => q1(token),
-        _ => false,
-    };
 }
